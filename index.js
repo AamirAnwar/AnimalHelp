960e5883474cdb86747c272f6b1da5eeb21961f6
Aamir Anwar <aamir.anwar021193@gmail.com>
2017-11-08 15:47:59 +0530

    Building routes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
 var mongoose = require("mongoose");
+const PORT = 3000;
+
+
 
 
 app.get("/", function(req,res) {
@@ -8,8 +11,17 @@ app.get("/", function(req,res) {
 
 });
 
-const PORT = 3000;
+app.get("/tags/:tagname", function(req,res) {
 
+
+	res.send("You are on the " + req.params.tagname + " page");
+
+});
+
+app.get("*", function(req,res) {
+	res.send("Nothing here!");
+
+});
 
 app.listen(PORT, function() {
 	console.log("Port started on " + PORT);
