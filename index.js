e190fcd3713b35826f92dfc141566f8c11e479bb
Aamir Anwar <aamir.anwar021193@gmail.com>
2017-11-08 16:09:27 +0530

    added get clinics route

--- index.js
+++ index.js
@@ -3,6 +3,39 @@ var app = express();
 var mongoose = require("mongoose");
 const PORT = 3000;
 
+// Setup DB
+
+mongoose.connect('mongodb://localhost/clinics');
+var Schema = mongoose.Schema, objectId = Schema.ObjectId;
+
+var clinicSchema = new Schema({
+	author:objectId,
+	name: String,
+	date: {type: Date, default: Date.now}
+
+});
+
+var ClinicModel = mongoose.model("clinic", clinicSchema)
+
+
+// DB Helpers
+
+const FindClinics = function(callback) {
+
+	ClinicModel.find({}).exec((function(err, docs) {
+		if (err) {
+			console.log(err);
+			callback(null);
+		}
+		else {
+			console.log('Found the following clinics');
+			console.log(docs);
+			callback(docs);
+			// callback(null);
+		}
+	}));
+
+}
 
 
 
@@ -11,12 +44,23 @@ app.get("/", function(req,res) {
 
 });
 
-app.get("/tags/:tagname", function(req,res) {
+app.get("/clinics", function(req,res){
+	FindClinics(function(clinics){
+		var names = [];
+		clinics.forEach(function(clinic) {
+			names.push(clinic.name);
+		})
 
-
-	res.send("You are on the " + req.params.tagname + " page");
-
+		if (clinics !== null) {
+			res.send('We have ' + names.length + ' clinics in our database');
+		}
+		else {
+			res.send("There was a problem at the backend");	
+		}
+		
+	});
 });
+
 
 app.get("*", function(req,res) {
 	res.send("Nothing here!");
